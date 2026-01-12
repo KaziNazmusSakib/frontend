0d2ce2bd3339ae32ca7ec0f52d297e2b480ba76a
KaziNazmusSakib <kazinazmussakib36@gmail.com>
2026-01-12 21:51:28 +0600

    Nexify-08: further update

---
+++ hooks/useAuth.ts
@@ -0,0 +1,11 @@
+// hooks/useAuth.ts
+import { useContext } from 'react'
+import AuthContext from '@/components/AuthProvider'
+
+export function useAuth() {
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return context
+}
